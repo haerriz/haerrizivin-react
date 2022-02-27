56745e8b5a86a84ea0b3028e0f06942e35d79575
Haerriz <haerriz@gmail.com>
2022-02-28 00:49:08 +0530

    added primary files

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {render} from "react-dom";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            string: 'Ivin',
+        }
+    }
+  render() {
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                      Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      Learn React
+                  </a>
+                  <p>{this.state.string}</p>
+                  <button onClick={() => this.setState({string: 'UNaku laam vanakam solla mudiyadhu'})}>Click me</button>
+              </header>
+          </div>
+      )
+  }
+}
+
+
+export default App;
