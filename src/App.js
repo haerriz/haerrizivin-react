260153d1a4240e28c18c99e3c8d5652e0e064335
Haerriz <haerriz@gmail.com>
2022-03-02 22:40:29 +0530

    Monsters added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
-import {render} from "react-dom";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            string: 'Ivin',
-        }
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  
   render() {
-      return (
-          <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>
-                      Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                      className="App-link"
-                      href="https://reactjs.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                  >
-                      Learn React
-                  </a>
-                  <p>{this.state.string}</p>
-                  <button onClick={() => this.setState({string: 'Unaku laam vanakam solla mudiyadhu'})}>Click me</button>
-              </header>
-          </div>
+     const { monsters, searchField } = this.state; 
+     const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
       )
+     return (
+      <div className='App'>
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+         placeholder='search monsters'
+         handleChange={this.handleChange}
+         />
+        <CardList monsters={ filteredMonsters } />
+      </div>
+    );
   }
 }
-
-
 export default App;
